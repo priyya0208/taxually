36365d84fd7ca1c729a20c28bc6bcfb08f67e83a
Priyanka Shrivastava <priyya.0208@gmail.com>
2022-11-23 13:13:03 +0000

    components, shared service and authguard file created

--- taxually/src/app/app.module.ts
+++ taxually/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
